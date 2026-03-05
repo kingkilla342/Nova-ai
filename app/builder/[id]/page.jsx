29b281271776bf899f1ecbf8fdf0037dbccf8c93
kingkilla342 <kingggpossible@gmail.com>
2026-03-05 00:56:41 -0500

    Delete app directory

--- app/builder/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import Builder from '../../../components/Builder';
-
-export default function BuilderPage({ params }) {
-  return <Builder projectId={params.id} />;
-}
--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import Dashboard from '../components/Dashboard';
-
-export default function Home() {
-  return <Dashboard />;
-}
